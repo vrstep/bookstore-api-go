63bf69e6d0dbb7a2dfb6b787125560abe504c474
Amir <amir.batyrggaliev@gmail.com>
2025-03-27 21:04:11 +0500

    call api functions

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"example/bookstore_api/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Books
+	router.GET("/books", handlers.GetBooks)
+	router.POST("/books", handlers.PostBooks)
+	router.GET("/books/:id", handlers.GetBookByID)
+
+	// Authors
+	router.GET("/authors", handlers.GetAuthors)
+	router.POST("/authors", handlers.PostAuthors)
+
+	// Categories
+	router.GET("/categories", handlers.GetCategories)
+	router.POST("/categories", handlers.PostCategories)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run("localhost:8080")
 }
