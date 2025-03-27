0bfe3170adf5978bac4cee3e4729c4fd5a57736c
Amir <amir.batyrggaliev@gmail.com>
2025-03-27 20:54:34 +0500

    setup bookstore models

---
+++ models/book.go
@@ -0,0 +1,9 @@
+package models
+
+type Book struct {
+	ID         int     `json:"id"`
+	Title      string  `json:"title"`
+	AuthorID   int     `json:"author_id"`
+	CategoryID int     `json:"category_id"`
+	Price      float64 `json:"price"`
+}
